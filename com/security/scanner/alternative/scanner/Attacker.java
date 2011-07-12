b3fd0b943ded1578dab3571ae3cf35ff3aa1396c
adyon2004@gmail.com <adyon2004@gmail.com@99054cb6-801b-9fc9-32c8-59f04fbbb571>
2011-07-12 23:46:19 +0000

    Change of heart. No more Junit tests. Go for public static void main approach

--- com/security/scanner/alternative/scanner/Attacker.java
+++ com/security/scanner/alternative/scanner/Attacker.java
@@ -1,25 +1,25 @@
 package com.security.scanner.alternative.scanner;
 
+import com.thoughtworks.selenium.Selenium;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.internal.seleniumemulation.Open;
 
 import java.util.List;
 
-public class Attacker implements Runnable{
+public class Attacker implements Runnable {
 
     String vector;
     String url;
     WebDriver driver;
 
-    public Attacker (WebDriver driver, String urlToAttack)
-    {
+    public Attacker(WebDriver driver, String urlToAttack) {
         this.driver = driver;
         this.url = urlToAttack;
     }
 
-    public Attacker using(String attackVector)
-    {
+    public Attacker using(String attackVector) {
         this.vector = attackVector;
         return this;
     }
@@ -29,17 +29,14 @@ public class Attacker implements Runnable{
         List<WebElement> allInputs;
         List<WebElement> allForms;
 
-        driver.get(url);
         allForms = driver.findElements(By.xpath("//form"));
-        for (WebElement form: allForms)
-        {
+        for (WebElement form : allForms) {
             allInputs = form.findElements(By.xpath(".//input"));
             allInputs.addAll(form.findElements(By.xpath(".//textarea")));
             for (WebElement input : allInputs) {
 
                 if (input.isDisplayed() && input.isEnabled() &&
-                        (input.getAttribute("type").equals("text") || input.getTagName().equals("textarea")))
-                {
+                        (input.getAttribute("type").equals("text") || input.getTagName().equals("textarea"))) {
                     input.sendKeys(vector);
                 }
             }
